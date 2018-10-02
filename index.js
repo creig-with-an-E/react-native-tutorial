d3544a9fd55718cbcc80eb3860729ba4c092dea0
Creig Fortune <fortunephiri004@gmail.com>
2018-10-02 14:45:13 -0400

    addition of index.js and AlbumList component that will be used to list the albums

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 //import a library to help create a component
 
 import React  from 'react';
-import {Text, AppRegistry } from 'react-native';
+import {Text, AppRegistry, View } from 'react-native';
+import Header from './src/components/header'
+import AlbumList from './src/components/AlbumList';
+
+
 //attempt to render a component
 
 const App =() => {
-       return( <Text>Some Text</Text>)
+       return(
+           <View>
+               <Header headerText={'Albums'}/>
+                <AlbumList></AlbumList>
+           </View>
+       )
     }
 ;
 
 //render to device
 
 AppRegistry.registerComponent('albums',()=>App);
+//only the root component uses the app registry
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,31 @@
-// import libraries;
 import React from 'react';
-import {Text} from 'react-native';
+import { Text ,View} from 'react-native';
 
+const Header =(props) =>{
+    //destructuring
 
-//create component
-const Header =() =>{
-    return <Text >Albums.!</Text>
-}
+    const { textStyle, viewStyle } = styles;
+    return (
+            <View style={viewStyle}>
+                <Text style={textStyle}>{props.headerText}</Text>
+            </View>
+           );
+};
 
-//make the comment available to other components
+const styles ={
+    viewStyle:{
+               backgroundColor: '#F8F8F8',
+               justifyContent:'center',
+               alignItems: 'center',
+               height: 60,
+               paddingTop:15,
+               shadowColor:'#000',
+               shadowOffset:{width:0, height:2},
+               shadowOpacity:0.2,
+               elevation:2,
+               position:'relative'
+    },
+    textStyle:{fontSize:20}
+};
+
 export default Header;
