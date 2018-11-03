f2e0fe765c77a94266ca2990fb7752a503b7519c
Creig Fortune <fortunephiri004@gmail.com>
2018-11-02 23:48:31 -0600

    adding state to the albums class component. the class starts off with an empty state object

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
-import {View , Text} from 'react-native';
+import { View , Text } from 'react-native';
+import axios from 'axios';  //used to make the http request
 
 class AlbumList extends Component {
+    //when to load the component, when the component is about to render we want to get the data
+    //class based component knows when its about to be rendered
+
+    state ={albums: []};
+    componentWillMount(){
+        axios.get("https://rallycoding.herokuapp.com/api/music_albums")
+           .then(response => console.log(response));
+    }
+
     render(){
         return(
             <View>
